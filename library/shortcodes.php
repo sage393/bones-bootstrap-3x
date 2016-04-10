80723045758de4206a32eb078b5debe08216d63f
sage393 <sage393@gmail.com>
2016-04-10 16:15:46 -0600

    se agrega shortcode para los post recientes

--- library/shortcodes.php
+++ library/shortcodes.php
@@ -2,9 +2,17 @@
 
 //[hello_world]
 function hello_world_func(){
-    require_once('features/hello_world.php');
+    return include('features/hello_world.php');
 }
 add_shortcode( 'hello_world', 'hello_world_func' );
+
+//------------------------------------------------------------------------------
+
+//[recent_post]
+function recent_post_func(){
+    return include('features/recent_post.php');
+}
+add_shortcode( 'recent_post', 'recent_post_func' );
 
 //------------------------------------------------------------------------------
 
