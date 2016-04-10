96229cdb9d8ef32a706f3258ccdd0d7248b2e762
sage393 <sage393@gmail.com>
2016-04-10 14:47:47 -0600

    se añadieron archivos de para segmentar el tema

---
+++ parts/nav/main-nav.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a class="navbar-brand" href="<?php bloginfo('url'); ?>">
+                <?php bloginfo('name'); ?>
+            </a>
+        </div>
+
+        <?php
+            wp_nav_menu( array(
+                'menu'              =>  __( 'The Main Menu', 'bonestheme' ),
+                'theme_location'    => 'main-nav',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse navbar-ex1-collapse',
+                'menu_class'        => 'nav navbar-nav navbar-right',
+                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                'walker'            => new wp_bootstrap_navwalker())
+            );
+        ?>
+    </div>
+</nav>
