9c91fbd5f60deadb30d638a1cc56bac2161bc054
sage393 <sage393@gmail.com>
2016-04-10 14:47:21 -0600

    se cambio url de main-nav.php y se añadio container

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
 
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
-		<div id="container">
+		<div id="container" class="container">
 
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
@@ -55,23 +55,10 @@
 					<?php // bloginfo('description'); ?>
 
 
-					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-						<?php wp_nav_menu(array(
-    					         'container' => false,                           // remove nav container
-    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-    					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
-    					         'theme_location' => 'main-nav',                 // where it's located in the theme
-    					         'before' => '',                                 // before the menu
-        			               'after' => '',                                  // after the menu
-        			               'link_before' => '',                            // before each link
-        			               'link_after' => '',                             // after each link
-        			               'depth' => 0,                                   // limit the depth of the nav
-    					         'fallback_cb' => ''                             // fallback function (if there is one)
-						)); ?>
-
-					</nav>
+					<!-- navbar -->
+					<?php require_once('parts/nav/main-nav.php'); ?>
 
 				</div>
 
 			</header>
+		</div>
